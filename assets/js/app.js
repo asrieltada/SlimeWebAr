695e5138beddae7e9456d1d1438cfd08d12aa143
Asriel Tada <asrieltada@gmail.com>
2022-06-13 16:56:36 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -67,6 +67,11 @@ class App{
 					name: 'slime',
 					npc: false
 				};
+
+                var newMaterial = new THREE.MeshStandardMaterial({color: 0xff0000});
+                self.slime.traverse((o) => {
+                    if (o.isMesh) o.material = newMaterial;
+                  });
 				
 				self.slime = new Player(options);
                 self.slime.object.visible = false;
